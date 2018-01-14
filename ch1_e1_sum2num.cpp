bcdacc34c32352fb8b0e6465a11f509fdd20aebc
xhuo <huoxiaoyu90@163.com>
2018-01-14 17:47:09 +0800

    fixed endl after cin.

--- ch1_e1_sum2num.cpp
+++ ch1_e1_sum2num.cpp
@@ -3,7 +3,7 @@ int main()
 {
 	std::cout << "Enter two numbers:" << std::endl;
 	int v1, v2;
-	std::cin >> v1 >> v2 >> std::endl;
+	std::cin >> v1 >> v2;
 	std::cout << "The sum of " << v1 << " and " << v2
 	     << " is " << v1 + v2 << std::endl;
 	return 0;
